fa674d75b1980558ff99c60065d45a25a0806560
Luciano Ramello <luchosr@gmail.com>
2025-04-13 23:00:25 +0200

    feat(Login): improves auth layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from './components/ui/button';
+import AuthLayout from './auth/layout/AuthLayout';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hola Mundo</h1>
-      <Button variant="default">Click me</Button>
+      <AuthLayout />
     </>
   );
 }
