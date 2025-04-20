be08c547e90bd21b438022114fc488dacdb13e1c
Luciano Ramello <luchosr@gmail.com>
2025-04-20 16:34:23 +0200

    feat(App) : mounts chat layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import AuthLayout from './auth/layout/AuthLayout';
+import ChatLayout from './chat/layout/ChatLayout';
+import ChatPage from './chat/layout/pages/ChatPage';
 
 function App() {
   return (
     <>
-      <AuthLayout />
+      <ChatLayout>
+        <ChatPage />
+      </ChatLayout>
     </>
   );
 }
