7ce812d5d0e5237367943a4e7b616c47a8b928ea
Luciano Ramello <luchosr@gmail.com>
2025-04-23 10:37:50 +0200

    feat(AppRouter):apply lazy loading

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,21 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import { AuthLayout } from './auth/layout/AuthLayout';
 import { LoginPage } from './auth/pages/LoginPage';
 import { RegisterPage } from './auth/pages/RegisterPage';
+// import ChatLayout from './chat/layout/ChatLayout';
+// import ChatPage from './chat/layout/pages/ChatPage';
+
+import { sleep } from './lib/sleep';
+
+const ChatPage = lazy(() => {
+  return import('./chat/layout/pages/ChatPage');
+});
+
+const ChatLayout = lazy(async () => {
+  await sleep(1500);
+  return import('./chat/layout/ChatLayout');
+});
 
 export default function AppRouter() {
   return (
@@ -12,6 +26,27 @@ export default function AppRouter() {
           <Route path="/auth/register" element={<RegisterPage />} />
         </Route>
         <Route path="/" element={<Navigate to="/auth" />} />
+        <Route
+          path="/chat"
+          element={
+            <Suspense
+              fallback={
+                <div className="flex justify-center items-center h-screen">
+                  <div
+                    className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full text-primary"
+                    role="status"
+                  >
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </div>
+              }
+            >
+              <ChatLayout />
+            </Suspense>
+          }
+        >
+          <Route index element={<ChatPage />} />
+        </Route>
 
         <Route path="*" element={<Navigate to="/auth" />} />
       </Routes>
