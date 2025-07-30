0e9bc2c0e86255ad0bbd531fd0ed8c796ad2f83e
Luciano <luchosr@gmail.com>
2025-07-30 22:49:35 +0200

    feat(chatPage): implements messages mutation on send

--- src/chat/pages/ChatPage.tsx
+++ src/chat/pages/ChatPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useParams } from 'react-router';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { ScrollArea } from '@/components/ui/scroll-area';
@@ -12,25 +13,50 @@ import {
   Send,
   MessageSquare,
 } from 'lucide-react';
-import { getClientMessages } from '@/fake/fake-data';
+
+import { getClientMessages, sendMessage } from '@/fake/fake-data';
+import { Message } from '../interfaces/chat.interface';
 
 export default function ChatPage() {
   const { clientId } = useParams();
-
+  const queryClient = useQueryClient();
   const [input, setInput] = useState('');
 
   const { data: messages = [], isLoading } = useQuery({
     queryKey: ['messages', clientId],
     queryFn: () => getClientMessages(clientId ?? ''),
-    enabled: !!clientId,
-    staleTime: 1000 * 60 * 5, // 5 minutes
   });
+
+  const { mutate: sendMessageMutation } = useMutation({
+    mutationFn: sendMessage,
+    onSuccess: (newMessage) => {
+      queryClient.setQueryData(
+        ['messages', clientId],
+        (oldMessages: Message[]) => [...oldMessages, newMessage]
+      );
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    sendMessageMutation({
+      clientId: clientId ?? '',
+      content: input,
+      createdAt: new Date(),
+      sender: 'agent',
+    });
+
+    setInput('');
+  };
 
   if (isLoading) {
     return (
-      <div className="flex items-center justify-center h-full">
-        <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-primary" />
-        <span className="ml-2">Loading...</span>
+      <div className="flex-1 flex items-center justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <div className="h-8 w-8 border-4 border-primary border-t-transparent rounded-full animate-spin" />
+          <p className="text-sm text-muted-foreground">Loading messages...</p>
+        </div>
       </div>
     );
   }
@@ -38,16 +64,17 @@ export default function ChatPage() {
   return (
     <div className="flex-1 flex flex-col">
       <ScrollArea className="flex-1 p-4">
-        {messages && messages.length === 0 && (
-          <div className="flex items-center  pb-10 justify-center text-muted-foreground">
-            <MessageSquare className="h-8 w-8 mr-2" />
-            No messages yet, start a conversation with the customer.
+        {messages.length === 0 && (
+          <div className="mt-10 flex-1 flex flex-col items-center justify-center gap-4">
+            <MessageSquare className="h-8 w-8 text-muted-foreground" />
+            <p className="text-sm text-muted-foreground">No hay mensajes</p>
           </div>
         )}
+
         <div className="space-y-4">
           {messages.map((message, index) => (
             <div key={index} className="w-full">
-              {message.sender === 'agent' ? (
+              {message.sender === 'client' ? (
                 // Agent message - left aligned
                 <div className="flex gap-2 max-w-[80%]">
                   <div className="h-8 w-8 rounded-full bg-primary flex-shrink-0" />
@@ -101,18 +128,20 @@ export default function ChatPage() {
       </ScrollArea>
 
       <div className="p-4 border-t">
-        <div className="flex items-center gap-2">
-          <Textarea
-            placeholder="Type a message as a customer"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            className="min-h-[44px] h-[44px] resize-none py-3"
-          />
-          <Button className="h-[44px] px-4 flex items-center gap-2">
-            <Send className="h-4 w-4" />
-            <span>Send</span>
-          </Button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="flex items-center gap-2">
+            <Textarea
+              placeholder="Type a message as a customer"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="min-h-[44px] h-[44px] resize-none py-3"
+            />
+            <Button className="h-[44px] px-4 flex items-center gap-2">
+              <Send className="h-4 w-4" />
+              <span>Send</span>
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   );
